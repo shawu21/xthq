935bac58ad76591bfd301ca967c212e7dfc36f32
luxuetao <2982271907@qq.com>
2021-11-30 12:32:57 +0800

    upload databaseconfig and mailconfig

--- config/database.go
+++ config/database.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func Tes() {
+func init() {
 	db, err := gorm.Open("mysql", "root:@(127.0.0.1:3306)/db?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println("err=", err)
