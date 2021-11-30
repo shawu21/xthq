f72fe0cfd5bfe1032b7cf357279c734b96d56650
luxuetao <2982271907@qq.com>
2021-11-30 12:42:07 +0800

    upload emailgo

--- controller/email.go
+++ controller/email.go
@@ -1,7 +1,26 @@
 package controller
-//使用gomail实现
-import "github.com/gin-gonic/gin"
 
-func SendEamil(c *gin.Context) {
+//使用gomail实现
+import (
+	"Test/config"
+	"fmt"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEamil(EmailAddress string) {
+	m := gomail.NewMessage()
+
+	mailConfig := config.GetMailConfig()
+
+	m.SetAddressHeader("From", mailConfig["username"].(string), "lxtbolog")
+	m.SetHeader("To", EmailAddress)
+	m.SetHeader("Subject", "你已回复")
+	m.SetBody("text/html", "回复成功")
+	send := gomail.NewDialer(mailConfig["host"].(string), mailConfig["port"].(int), mailConfig["username"].(string), mailConfig["password"].(string))
+	err := send.DialAndSend(m)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
 
 } //发送邮件以此表明已经回复对方邮件，使用go同时进行
